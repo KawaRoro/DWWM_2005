f6159c2c4d153130185a8e5a9301f68810ddd9a0
KawaRoro <webmaster.benjamin@gmail.com>
2020-05-07 14:12:40 +0200

    Exercice 3 -> ok

--- FOAD_Algo_Java/src/com/schoenmaeker/Algo_3_3.java
+++ FOAD_Algo_Java/src/com/schoenmaeker/Algo_3_3.java
@@ -8,11 +8,11 @@ package com.schoenmaeker;
  * @since 2020
  *
  * //----------------------------------------------------//
- * // Rechercher une lettre dans une chaine de caractères
+ * // Dénombrer les lettres de l'alphabet dans un tableau
  * // Voir FOAD_Algo
  */
 
-public class Algo_3_2 {
+public class Algo_3_3 {
 	
 	/**
 	 * @param args
@@ -20,22 +20,35 @@ public class Algo_3_2 {
 	// Exercice le périmétre d'un rectangle
 	public static void main(String[] args) {
 		
-		String str = "abc.";
-		int cpt_occ = 0; // compteur occurences
-		int longeur_str = str.length();
-		char letter;
+		String str_120_caract = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus consequat elementum. Pellentesque pulvinar tellus.";
+		int length_str = str_120_caract.length();
+		String[] tab_alphabet = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+		int length_tab_alphabet = tab_alphabet.length;
+		int[] tab_cpt_occ = new int[26];
 		
-		for(int i = 0; i < longeur_str ; i++ ) {
-			letter = str.charAt(i);
-			if(letter == 'a') {
-				cpt_occ++;
+		if(length_str >= 120) {
+			
+			// Deux boucles une pour le texte à lire et une autre pour l'alhpabet à vérifier
+			for(int i = 0; i < length_str ; i++ ) {
+				char charAtNumber = str_120_caract.charAt(i);
+				
+				for(int f = 0; f < length_tab_alphabet ; f++ ) {
+					
+					if(tab_alphabet[f].equals(new String(new char[]{charAtNumber}))) {
+						tab_cpt_occ[f] = tab_cpt_occ[f]+1;
+						//System.out.println(tab_cpt_occ[f]);
+					}
+				}
+				
+				
 			}
-		}
-		
-		if(str == ".") {
-			System.out.println("LA CHAINE EST VIDE");
-		}else {
-			System.out.println("Le nombre de a contenu dans la chaine est de "+cpt_occ);
+			
+			// Affichage du compte rendu
+			for(int e = 0; e < length_tab_alphabet ; e++ ) {
+				
+				System.out.println( "La lettre " +tab_alphabet[e] + " est présent " + tab_cpt_occ[e] + " fois");
+				
+			}
 		}
 		
 	}
