90df7d2fd73aa8ab046769b1d0f50e8262da29bb
KawaRoro <webmaster.benjamin@gmail.com>
2020-05-07 13:45:17 +0200

    petite correction

---
+++ FOAD_Algo_Java/src/com/schoenmaeker/Algo_3_3.java
@@ -0,0 +1,43 @@
+package com.schoenmaeker;
+
+//import java.util.Scanner;
+
+/**
+ * @author Schoenmaeker
+ * @version 0.0.1
+ * @since 2020
+ *
+ * //----------------------------------------------------//
+ * // Rechercher une lettre dans une chaine de caractères
+ * // Voir FOAD_Algo
+ */
+
+public class Algo_3_2 {
+	
+	/**
+	 * @param args
+	 */
+	// Exercice le périmétre d'un rectangle
+	public static void main(String[] args) {
+		
+		String str = "abc.";
+		int cpt_occ = 0; // compteur occurences
+		int longeur_str = str.length();
+		char letter;
+		
+		for(int i = 0; i < longeur_str ; i++ ) {
+			letter = str.charAt(i);
+			if(letter == 'a') {
+				cpt_occ++;
+			}
+		}
+		
+		if(str == ".") {
+			System.out.println("LA CHAINE EST VIDE");
+		}else {
+			System.out.println("Le nombre de a contenu dans la chaine est de "+cpt_occ);
+		}
+		
+	}
+	
+}
