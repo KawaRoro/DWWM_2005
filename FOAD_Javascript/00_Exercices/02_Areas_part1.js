31c36f361db3dd79b74a8d65aaa5fe36a4022d62
KawaRoro <webmaster.benjamin@gmail.com>
2020-05-28 09:10:19 +0200

    rabbit

--- FOAD_Javascript/00_Exercices/02_Areas_part1.js
+++ FOAD_Javascript/00_Exercices/02_Areas_part1.js
@@ -21,96 +21,98 @@
  /**
   * La classe "Point" représente les coordonnées d'un point dans une zone à 2 dimensions
   */
-class Point 
-{
-    /**
-     * Constructeur: Initialise une nouvelle instance de la classe "Point"
-     * @param int _x Coordonnée horizontale du point (abscisse). Valeur négative acceptée
-     * @param int _y Coordonnée verticale du point (ordonnée). Valeur négative acceptée
-     */
-    constructor(_x, _y) {
-        this.x = parseFloat(_x); // 
-        this.y = parseFloat(_y); //
-    }
-
-    move(_x, _y) {
-        this.x = _x;
-        this.y = _y;
-    }
-
-    duplicate() {
-        const duplicate = new Point(this.x, this.y);
-        return duplicate;
-    }
-
-    copy(_point) {
-        this.x =  _point.x; // target <- source
-        this.y =  _point.y; // target <- source
-        return this;
-    }
-
-    rabbit(_point) {
-       /*
-        let save_point_x = this.x;
-        let save_point_y = this.y;
-
-        this.x = _point.x;
-        this.y = _point.y;
-
-        _point.x = save_point_x;
-        _point.y = save_point_y;
-        */
-        
-        let save_obj = new Point(this.x, this.y);
-        this = _point;
-        return save_obj;
-    }
-
-    /**
-     * Retourne une représentation textuelle du Point
-     * @return string Les coordonnées du Point
-     */
-    toString() {
-        return "(" + this.x + "," + this.y + ")";
-    }
-
-}
-
-
-
-/**
- * Programme "Point"
- * Décrivez ci-dessous en quelques lignes le programme suivant
- * (utilité, finalité)
- * 
- * 
- * 
- * 
- * 
- */
-
-let p1 = new Point(0, 1);
-let p2 = new Point(2, 3);
-let p3 = new Point(4, 5);
-let p4;
-let p5;
-
-console.log(p1);     // affiche: Point { x: 0, y: 1 }
-console.log(p2);     // affiche: Point { x: 2, y: 3 }
-console.log(p3);     // affiche: Point { x: 4, y: 5 }
-
-p4 = p3.duplicate();
-console.log(p3 !== p4);         // affiche: true
-console.log(p3.toString());     // affiche (4,5)
-console.log(p4.toString());     // affiche: (4,5)
-
-p2.copy(p1);
-console.log(p1 !== p2);         // affiche: true
-console.log(p1.toString());     // affiche:  (0,1)
-console.log(p2.toString());     // affiche:  (0,1)
-
-p1.rabbit(p3);
-console.log(p1 !== p3);         // affiche: true
-console.log(p1.toString());     // affiche:  (4,5)
-console.log(p3.toString());     // affiche:  (0,1)
-/**/
+ class Point 
+ {
+     /**
+      * Constructeur: Initialise une nouvelle instance de la classe "Point"
+      * @param int _x Coordonnée horizontale du point (abscisse). Valeur négative acceptée
+      * @param int _y Coordonnée verticale du point (ordonnée). Valeur négative acceptée
+      */
+     constructor(_x, _y) {
+         this.x = parseFloat(_x); // 
+         this.y = parseFloat(_y); // parseFloat(_y)
+     }
+ 
+     move(_x, _y) {
+         this.x = _x;
+         this.y = _y;
+     }
+ 
+     duplicate() {
+         const duplicate = new Point(this.x, this.y);
+         return duplicate;
+     }
+ 
+     copy(_point) {
+         this.x =  _point.x; // target <- source
+         this.y =  _point.y; // target <- source
+         return this;
+     }
+ 
+     rabbit(_point) {
+        /*
+         let save_point_x = this.x;
+         let save_point_y = this.y;
+ 
+         this.x = _point.x;
+         this.y = _point.y;
+ 
+         _point.x = save_point_x;
+         _point.y = save_point_y;
+         */
+         
+        let save_point = new Point(this.x, this.y);
+        this.copy(_point);
+        _point.copy(save_point);
+        //return save_point;
+     }
+ 
+     /**
+      * Retourne une représentation textuelle du Point
+      * @return string Les coordonnées du Point
+      */
+     toString() {
+         return "(" + this.x + "," + this.y + ")";
+     }
+ 
+ }
+ 
+ 
+ 
+ /**
+  * Programme "Point"
+  * Décrivez ci-dessous en quelques lignes le programme suivant
+  * (utilité, finalité)
+  * 
+  * 
+  * 
+  * 
+  * 
+  */
+ 
+ let p1 = new Point(0, 1);
+ let p2 = new Point(2, 3);
+ let p3 = new Point(4, 5);
+ let p4;
+ let p5;
+ 
+ console.log(p1);     // affiche: Point { x: 0, y: 1 }
+ console.log(p2);     // affiche: Point { x: 2, y: 3 }
+ console.log(p3);     // affiche: Point { x: 4, y: 5 }
+ 
+ p4 = p3.duplicate();
+ console.log(p3 !== p4);         // affiche: true
+ console.log(p3.toString());     // affiche (4,5)
+ console.log(p4.toString());     // affiche: (4,5)
+ 
+ p2.copy(p1);
+ console.log(p1 !== p2);         // affiche: true
+ console.log(p1.toString());     // affiche:  (0,1)
+ console.log(p2.toString());     // affiche:  (0,1)
+ 
+ p1.rabbit(p3);
+ console.log(p1 !== p3);         // affiche: true
+ console.log(p1.toString());     // affiche:  (4,5)
+ console.log(p3.toString());     // affiche:  (0,1)
+ /**/
+ 
